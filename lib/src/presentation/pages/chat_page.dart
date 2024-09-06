fa83a630d4e18ff177b7bb28d179384d38276bb3
ciriti <ciriti@gmail.com>
2024-09-06 12:05:38 +0200

    apikey (#2)

--- lib/src/presentation/pages/chat_page.dart
+++ lib/src/presentation/pages/chat_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:ztc/src/application/services/chat_service.dart';
 import 'package:ztc/src/presentation/widgets/app_drawer.dart';
 import 'package:ztc/src/utils/app_sizes.dart';
@@ -13,13 +14,37 @@ class ChatPage extends ConsumerStatefulWidget {
 
 class _ChatPageState extends ConsumerState<ChatPage> {
   final TextEditingController _textController = TextEditingController();
-  bool hasApiKey = true; // Replace with your actual logic to check for API key
+  bool hasApiKey = false; // Track if API key is set
+  String apiKey = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadApiKey(); // Load API key when the page is initialized
+  }
+
+  Future<void> _loadApiKey() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    apiKey = prefs.getString('apiKey') ?? '';
+    setState(() {
+      hasApiKey = apiKey.isNotEmpty;
+    });
+  }
 
   void _handleSubmitted(String message) {
-    if (message.isNotEmpty) {
+    if (message.isNotEmpty && hasApiKey) {
       ref.read(chatServiceProvider.notifier).sendMessage(message);
       _textController.clear(); // Clear the text field after submission
+    } else {
+      // Optionally show a message if there's no API key
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Please set an API key in the settings.')),
+      );
     }
+  }
+
+  void _reloadPage() {
+    _loadApiKey(); // Reload the API key and update the UI
   }
 
   @override
@@ -30,7 +55,7 @@ class _ChatPageState extends ConsumerState<ChatPage> {
       appBar: AppBar(
         title: const Text('Chatbot'),
       ),
-      drawer: const AppDrawer(),
+      drawer: AppDrawer(onApiKeyUpdated: _reloadPage),
       body: Column(
         children: [
           // Display the chat messages
@@ -76,15 +101,13 @@ class _ChatPageState extends ConsumerState<ChatPage> {
                       ),
                       suffixIcon: IconButton(
                         icon: const Icon(Icons.send),
-                        onPressed: !hasApiKey
-                            ? null
-                            : () => _handleSubmitted(_textController.text),
+                        onPressed: hasApiKey
+                            ? () => _handleSubmitted(_textController.text)
+                            : null, // Disable button if API key is not set
                       ),
                     ),
                   ),
                 ),
-
-                // Send IconButton
               ],
             ),
           ),
